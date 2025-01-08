af9d2f7aa3d8b2f55199647057fb7925bac35e8d
Camila Sarra <camilasarra@gmail.com>
2025-01-08 14:40:11 -0300

    front end routes

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/work">Work</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about-page">
+      <h1>About Me</h1>
+      <p>Hi! I'm Camila, a creative developer and illustrator...</p>
+      {/* Puedes añadir una sección para hablar de ti */}
+    </div>
+  );
+};
+
+export default About;
