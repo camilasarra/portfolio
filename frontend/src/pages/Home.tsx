5a0de621e8bb42ef8e65bc1165989d104d9d0cbe
Camila Sarra <camilasarra@gmail.com>
2025-01-22 15:16:06 -0300

    media query

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,25 +10,30 @@ const HomeContainer = styled.section`
   text-align: center;
   background-color: ${(props) => props.theme.colors.primary};
   color: ${(props) => props.theme.colors.secondary};
+  padding: 2rem;
+
+  @media (max-width: 768px) {
+    height: auto;
+    padding: 4rem 1.5rem;
+  }
 `;
 
 const Title = styled.h1`
   font-size: ${(props) => props.theme.fontSizes.large};
   font-family: ${(props) => props.theme.fonts.secondary};
+
+  @media (max-width: 768px) {
+    font-size: ${(props) => props.theme.fontSizes.medium};
+  }
 `;
 
 const Subtitle = styled.p`
   font-size: ${(props) => props.theme.fontSizes.medium};
   margin-top: 1rem;
-`;
 
-const Home = () => {
-  return (
-    <HomeContainer>
-      <Title>Turning coffee into experiences, and doodles into design</Title>
-      <Subtitle>Designer | Developer | Creative Mind</Subtitle>
-    </HomeContainer>
-  );
-};
+  @media (max-width: 768px) {
+    font-size: ${(props) => props.theme.fontSizes.small};
+  }
+`;
 
 export default Home;
